83c7ca62a5ad6f3d58dce5613af0baa1409659cc
Chukwudi <chukwudiojuro@outlook.com>
2025-12-29 15:26:53 -0400

    audio plays on countdown complete
    When the countdown reaches 0, an audio plays which the user can pause.

--- components/countdown-timer.tsx
+++ components/countdown-timer.tsx
@@ -1,15 +1,20 @@
 "use client"
 import { useEffect, useRef, useState } from 'react'
 import { Card, CardFooter, CardTitle } from './ui/card'
+import { Button } from './ui/button'
+import { Pause } from 'lucide-react'
 
-type Props = {
-  onComplete?: () => void
-}
+
 
 function getNextNewYear(): Date {
   const now = new Date()
   const year = now.getFullYear() + 1
-  return new Date(`January 1, ${year} 00:00:00`)
+  
+  
+  now.setSeconds(now.getSeconds()+20)
+  return now
+
+  // return new Date(`January 1, ${year} 00:00:00`)
 }
 
 function calculateTimeLeft(target: Date) {
@@ -28,14 +33,23 @@ function pad(value: number) {
   return value.toString().padStart(2, '0')
 }
 
-export default function NewYearCountdown({ onComplete }: Props) {
+
+
+export default function NewYearCountdown() {
   const targetDate = useRef(getNextNewYear())
   const hasCompleted = useRef(false)
+  const audioRef = useRef<HTMLAudioElement>(null)
 
+  
   const [isClient, setIsClient] = useState(false)
+  const [isLast10Seconds,setIsLast10Seconds] = useState(false)
   const [timeLeft, setTimeLeft] = useState(
     calculateTimeLeft(targetDate.current)
   )
+
+  function onComplete(){
+    audioRef.current?.play()
+  }
 
   useEffect(() => {
     setIsClient(true)
@@ -44,6 +58,13 @@ export default function NewYearCountdown({ onComplete }: Props) {
       const updated = calculateTimeLeft(targetDate.current)
       setTimeLeft(updated)
 
+      if (updated.hours === 0 &&
+        updated.minutes === 0 &&
+        updated.seconds <= 10
+      ){
+        setIsLast10Seconds(true)
+      }
+
       if (
         !hasCompleted.current &&
         updated.hours === 0 &&
@@ -51,13 +72,13 @@ export default function NewYearCountdown({ onComplete }: Props) {
         updated.seconds === 0
       ) {
         hasCompleted.current = true
-        onComplete?.()
+        onComplete()
         clearInterval(interval)
       }
     }, 1000)
 
     return () => clearInterval(interval)
-  }, [onComplete])
+  }, [])
 
   if (!isClient) {
     return null
@@ -65,21 +86,50 @@ export default function NewYearCountdown({ onComplete }: Props) {
 
   return (
     <Card
-    className='flex flex-col gap-4 justify-center items-center w-fit px-5
+    className='flex flex-col gap-4 justify-center items-center w-96 text-4xl px-5
     fixed left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2' >
-      <CardTitle>
+      <CardTitle
+      className=''>
+        <audio ref={audioRef} src="/audio/test.mp3"/>
+        <div
+        className=''>
 
-        <h1
-        className='text-6xl'>
-            {pad(timeLeft.hours)} : {pad(timeLeft.minutes)} : {pad(timeLeft.seconds)}
-        </h1>
+          {
+            hasCompleted.current ?
+            <h2>
+              HAPPY NEW YEAR
+            </h2>
+            :
+            isLast10Seconds?
+            <div>
+              <h1
+              className='text-9xl'>
+                {pad(timeLeft.seconds)}
+              </h1>
+            </div>
+            :
+            <div className='flex flex-col items-center justify-center gap-4'>
+              <h1>
+                {pad(timeLeft.hours)} : {pad(timeLeft.minutes)} : {pad(timeLeft.seconds)}
+              </h1>
+
+              <p className='text-2xl text-center'>
+                Until {new Date().getFullYear() + 1}
+              </p>
+            </div>
+          }
+          
+        </div>
+
       </CardTitle>
-
-      <CardFooter>
-        <p>
-            Until {new Date().getFullYear() + 1}
-        </p>
-      </CardFooter>
+      {
+        hasCompleted.current?
+          <Button onClick={()=>audioRef.current?.pause()}
+          className=''>
+            <Pause/>
+          </Button>
+        :null
+      }
     </Card>
   )
 }
